4b69821376501faa6d57ff837449061908404435
wyvern8 <wyvern@smartcreations.com.au>
2018-08-27 19:03:49 +1000

    fix(compile): typescript fixes

--- generators/package/templates/src/index.ts
+++ generators/package/templates/src/index.ts
@@ -1,18 +1,20 @@
 'use strict';
 
 import 'source-map-support/register';
-//import * as AWS from 'aws-sdk';
 import * as bunyan from 'bunyan';
-import * as express from 'express'
-let log = bunyan.createLogger({name: '<%= name %>'});
+import * as express from 'express';
+// import * as AWS from 'aws-sdk';
+// import * as json from 'format-json';
 
+const log = bunyan.createLogger({ name: 'micro-one' });
 const app = express();
+const port = process.env.SVC_PORT || 8088;
 
-app.get('/', (req, res) => {
-    res.send('response from <%= name %>')
+app.get('/', (req: any, res: any) => {
+    log.info(req.headers);
+    res.json({ message: 'response from micro-one' });
 });
 
-app.listen(8088, () => {
-    log.info('app listening on port 8088')
+app.listen(port, () => {
+    log.info(`app listening on port ${port}`);
 });
-
