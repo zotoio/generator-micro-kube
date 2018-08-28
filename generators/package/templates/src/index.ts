a5cce60013e50a8ac15530b972d32f15a4d45356
wyvern8 <wyvern@smartcreations.com.au>
2018-08-28 12:34:19 +1000

    fix(template): update defaults

--- generators/package/templates/src/index.ts
+++ generators/package/templates/src/index.ts
@@ -6,13 +6,13 @@ import * as express from 'express';
 // import * as AWS from 'aws-sdk';
 // import * as json from 'format-json';
 
-const log = bunyan.createLogger({ name: 'micro-one' });
+const log = bunyan.createLogger({ name: '<%= name %>' });
 const app = express();
 const port = process.env.SVC_PORT || 8088;
 
 app.get('/', (req: any, res: any) => {
     log.info(req.headers);
-    res.json({ message: 'response from micro-one' });
+    res.json({ message: 'response from <%= name %>' });
 });
 
 app.listen(port, () => {
